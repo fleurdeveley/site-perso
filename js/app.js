6af364c13b1352575a013ad4acb725c4e2e97f0c
fleur <fleurdeveley@gmail.com>
2021-12-29 11:51:29 +0100

    card-body

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     var httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
@@ -12,67 +12,85 @@
 
     function getProjects() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-            var projects = JSON.parse(httpRequest.responseText);
-            projects.forEach(project => {
-                console.log(project);
+            if (httpRequest.status === 200) {
+                var projects = JSON.parse(httpRequest.responseText);
+                projects.forEach(project => {
+                    var col = document.createElement("div");
+                    col.classList.add("col-md-12", "col-lg-6");
 
-                var col = document.createElement("div");
-                col.classList.add("col-md-12", "col-lg-6");
+                    var card = document.createElement("div");
+                    card.classList.add("card", "border-dark", "mb-3");
 
-                var card = document.createElement("div");
-                card.classList.add("card", "border-dark", "mb-3");
+                    var h4 = document.createElement("h4");
+                    h4.classList.add("card-header");
+                    h4.innerText = project.title;
+                    card.appendChild(h4);
 
-                var h4 = document.createElement("h4");
-                h4.classList.add("card-header");
-                h4.innerText = project.title;
-                card.appendChild(h4);
+                    var img = new Image();
+                    img.src = 'img/' + project.image;
+                    img.classList.add("card-img-top");
+                    card.appendChild(img);
 
-                var img = new Image();
-                img.src = 'img/' + project.image;
-                img.classList.add("card-img-top");
-                card.appendChild(img);
+                    var cardBody = document.createElement("div");
+                    cardBody.classList.add("card-body");
 
-                var cardFooter = document.createElement("div");
-                cardFooter.classList.add("card-footer");
+                    var cardText = document.createElement("p");
+                    cardText.classList.add("card-text", "text-center");
+                    cardText.innerText = project.description;
+                    cardBody.appendChild(cardText);
 
-                var cardFooterContent = document.createElement('div');
-                cardFooterContent.classList.add("d-flex", "justify-content-around");
+                    var cardBodyContent = document.createElement('div');
+                    cardBodyContent.classList.add("d-flex", "flex-wrap", "justify-content-around");
 
-                if(project.url_site.length >0) {
-                    var demo = document.createElement("a");
-                    demo.href = project.url_site;
-                    demo.setAttribute("target", "_blank");
-                    demo.classList.add("btn");
-                    demo.innerText = "Démo";
+                    project.competences.forEach(competence => {
+                        var h5 = document.createElement("h5");
+                        var span = document.createElement("span");
+                        span.classList.add("badge", "bg-danger");
+                        span.innerText = competence.name;
+                        h5.appendChild(span);
+                        cardBodyContent.appendChild(h5);
+                    })
                     
-                    cardFooterContent.appendChild(demo);
-                }
+                    cardBody.appendChild(cardBodyContent);
+                    card.appendChild(cardBody);
 
-                if(project.url_github.length > 0) {
-                    var gitHub = document.createElement("a");
-                    gitHub.href = project.url_github;
-                    gitHub.setAttribute("target", "_blank");
-                    gitHub.classList.add("btn");
-                    gitHub.innerText = "GitHub";   
-                
-                    cardFooterContent.appendChild(gitHub);
-                }
+                    var cardFooter = document.createElement("div");
+                    cardFooter.classList.add("card-footer");
 
-                cardFooter.appendChild(cardFooterContent);
-                card.appendChild(cardFooter);
+                    var cardFooterContent = document.createElement('div');
+                    cardFooterContent.classList.add("d-flex", "justify-content-around");
 
-                col.appendChild(card);
+                    if (project.url_site.length > 0) {
+                        var demo = document.createElement("a");
+                        demo.href = project.url_site;
+                        demo.setAttribute("target", "_blank");
+                        demo.classList.add("btn");
+                        demo.innerText = "Site";
 
-                var row = document.getElementById('projects');
-                row.appendChild(col);
+                        cardFooterContent.appendChild(demo);
+                    }
 
+                    if (project.url_github.length > 0) {
+                        var gitHub = document.createElement("a");
+                        gitHub.href = project.url_github;
+                        gitHub.setAttribute("target", "_blank");
+                        gitHub.classList.add("btn");
+                        gitHub.innerText = "GitHub";
 
+                        cardFooterContent.appendChild(gitHub);
+                    }
 
-            }); 
-          } else {
-            console.log('Il y a eu un problème avec la requête.');
-          }
+                    cardFooter.appendChild(cardFooterContent);
+                    card.appendChild(cardFooter);
+
+                    col.appendChild(card);
+
+                    var row = document.getElementById('projects');
+                    row.appendChild(col);
+                });
+            } else {
+                console.log('Il y a eu un problème avec la requête.');
+            }
         }
     }
 })();
